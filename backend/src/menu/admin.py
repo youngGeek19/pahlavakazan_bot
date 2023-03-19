48b90d767c5bedc8f0ab6c169d79d8a762f71464
lenar19 <lenar.rashitov@internet.ru>
2023-03-19 18:45:53 +0500

    Created admin-panel && bot module

---
+++ backend/src/menu/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+
+from . import models
+
+
+
+class productsAdmin(admin.ModelAdmin):
+	list_display = ['pk', 'name', 'desc', 'price', 'photo']
+	list_display_links = ('name',)
+	list_editable = ['desc', 'price']
+	search_fields = ('name',)
+
+class clientsAdmin(admin.ModelAdmin):
+	list_display = ['pk', 'name', 'tel', 'email']
+	list_display_links = ('name',)
+	search_fields = ('name',)
+
+class ordersAdmin(admin.ModelAdmin):
+	list_display = ['pk', 'get_client_name', 'get_client_tel', 'product', 'date', 'is_active']
+	list_display_links = ('get_client_name', 'product')
+	search_fields = ('client', 'product')
+	date_hierarchy = 'date'
+	ordering = ['-date']
+	def get_client_name(self, obj):
+		return obj.client.name
+	def get_client_tel(self, obj):
+		return obj.client.tel
+	get_client_name.short_description = 'Клиент'
+	get_client_tel.short_description = 'Телефон'
+
+admin.site.register(models.Product, productsAdmin)
+admin.site.register(models.Client, clientsAdmin)
+admin.site.register(models.Order, ordersAdmin)
+
---
+++ backend/src/menu/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+
+class Client(models.Model):
+	name = models.CharField(verbose_name = 'Имя', max_length = 50)
+	tel = models.CharField(verbose_name = 'Телефон', max_length = 15)
+	email = models.EmailField(verbose_name = 'Эл. почта', blank=True, max_length = 50)
+
+
+	def __str__(self):
+		return self.name
+	class Meta:
+		verbose_name = 'Клиент'
+		verbose_name_plural = 'Клиенты'
+
+
+class Product(models.Model):
+	name = models.CharField(verbose_name = 'Название', max_length = 50)
+	desc = models.TextField(verbose_name = 'Описание', max_length = 250)
+	price = models.PositiveSmallIntegerField(verbose_name = 'Цена за кг')
+	photo = models.ImageField(upload_to = 'media/',verbose_name = 'Изображение')
+
+
+	def __str__(self):
+		return self.name
+	class Meta:
+		verbose_name = 'Продукт'
+		verbose_name_plural = 'Продукты'
+
+class Order(models.Model):
+	CHOICES = [
+		('active', 'Активный'),
+		('deactive', 'Неактивый')
+	]
+	client = models.ForeignKey(Client, verbose_name = 'Клиент', on_delete=models.CASCADE, related_name = 'client')
+	product = models.ForeignKey(Product,verbose_name = 'Продукт', on_delete=models.CASCADE, related_name = 'product')
+	date = models.DateTimeField(verbose_name = 'Дата заказа', auto_now = True)
+	is_active = models.CharField(verbose_name = 'Статус', choices = CHOICES, max_length = 10)
+
+	def __str__(self):
+		return self.client.name 
+	class Meta:
+		verbose_name = 'Заявка'
+		verbose_name_plural = 'Заявки'
+
+
